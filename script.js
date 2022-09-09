5b5f8190bdf6733e2d7109f4e0b77c1c37434f61
Matt <matt@stonebraker.io>
2022-09-09 11:29:07 -0400

    screen

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log('hi')
+// buttons
+const checksButton = document.getElementById('checks')
+const linesButton = document.getElementById('lines')
+const starsButton = document.getElementById('stars')
+
+// screen container
+const screen = document.getElementById('screen')
+
+console.log(screen)
